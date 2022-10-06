d27c11160fcb8d9a83b9b6b060ca5d76c3d46387
wveona <ms05114s@naver.com>
2022-10-06 11:57:21 +0900

    1

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDWsILA7cCh3ZWhrjsruJkCqffGmxjiFGPTrRbA3jiYtCGc9Hiw2Cab0-9F_hYS1MxC4&usqp=CAU'
+    })
+
+},500)
