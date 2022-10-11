52d312f2285417544509cf8c2b3c6fa892d5f6d3
wveona <ms05114s@naver.com>
2022-10-11 14:32:22 +0900

    sangsu

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ setInterval(()=>{
 
     let imgs = document.querySelectorAll('img');
     imgs.forEach((a, i)=>{
-        a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgDWsILA7cCh3ZWhrjsruJkCqffGmxjiFGPTrRbA3jiYtCGc9Hiw2Cab0-9F_hYS1MxC4&usqp=CAU'
+        a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH5mNrLmU2MV8IcFlN_Jgt-BsneiRimdOdzw&usqp=CAU'
     })
+
 
 },500)
